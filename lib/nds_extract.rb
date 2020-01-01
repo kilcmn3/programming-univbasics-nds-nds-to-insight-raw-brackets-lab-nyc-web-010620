4e07f4aa3edc6d0ee2f5adbab6a9d5cf8fa60702
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-01 22:09:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,7 @@ def directors_totals(nds)
     direcotr_name = nds[row_index][:name]
     direcotr_gross = nds[row_index][:movies]
 
-    result = {"#{direcotr_name}"}
+    result = {"#{direcotr_name}" => direcotr_gross }
     column_index = 0;
     total_gross = 0;
     while column_index < direcotr_gross.count do
